875a9235045a2e4a4e236eb6a31be47d0ff2ad21
yibing <zapplea@gmail.com>
2019-03-10 16:56:59 +0800

    add

--- generate_train_dataset.sh
+++ generate_train_dataset.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python3 create_pretraining_aic2018data.py \
-        --is_create_vocab False \
         --input_file '/datastore/liu121/sentidata2/data/meituan_jieba/testa_cut.pkl,/datastore/liu121/sentidata2/data/meituan_jieba/testb_cut.pkl,/datastore/liu121/sentidata2/data/meituan_jieba/train_cut.pkl,/datastore/liu121/sentidata2/data/meituan_jieba/val_cut.pkl' \
         --output_file '/datastore/liu121/sentidata2/data/bert/aic2018_pertaining_data.bert' \
         --vocab_file '/datastore/liu121/sentidata2/data/bert/vocab.txt'
